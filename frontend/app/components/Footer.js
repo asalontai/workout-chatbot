e154561d7eb2dcb7dcd8450ea17fb86a91664967
asalontai <93924842+asalontai@users.noreply.github.com>
2024-08-15 17:39:32 -0400

    adjusted footer

--- frontend/app/components/Footer.js
+++ frontend/app/components/Footer.js
@@ -3,7 +3,8 @@ import { Box, Typography } from "@mui/material";
 export default function Footer() {
     return (
         <Box 
-            component="footer"
+            position="fixed"
+            bottom={0}
             bgcolor="#2D2D2D"
             color="white"
             padding={2}
@@ -11,7 +12,7 @@ export default function Footer() {
             sx={{ position: 'realtive', marginTop: "auto", opacity: 0.8, }}
         >
             <Typography variant="body2">
-                &copy; {new Date().getFullYear()} WorkoutAI. All rights reserved.
+                &copy; {new Date().getFullYear()} WorkoutAI. All rights reserved. Version 1.0
             </Typography>
         </Box>
     )
